10e5a6767d93f4312fb7999b8520c39d52f2e723
mrx7355608 <mrx7355608@gmail.com>
2022-12-23 23:52:50 +0500

    admin page routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import AuthProvider from "./Contexts/AuthContext";
 // Pages
 import Welcome from "./Pages/Welcome";
 const Admin = React.lazy(() => import("./Pages/Admin"));
+const AdminDashboardUsers = React.lazy(() => import("./Pages/AdminDashboardUsers"));
+const AdminDashboardTests = React.lazy(() => import("./Pages/AdminDashboardTests"));
+const AdminDashboardLectures = React.lazy(() => import("./Pages/AdminDashboardLectures"));
 const Login = React.lazy(() => import("./Pages/Login"));
 const Register = React.lazy(() => import("./Pages/Register"));
 // Layouts
 import FormLayout from "./Layouts/FormLayout";
 import MainLayout from "./Layouts/MainLayout";
+import AdminLayout from "./Layouts/AdminLayout";
 
 function App() {
     return (
@@ -18,7 +22,12 @@ function App() {
                 <Routes>
                     <Route path="/" element={<MainLayout />}>
                         <Route index element={<Welcome />} />
-                        <Route path="admin" element={<Admin />} />
+                    </Route>
+                    <Route path="admin" element={<AdminLayout />}>
+                        <Route index element={<Admin />} />
+                        <Route path="users" element={<AdminDashboardUsers />} />
+                        <Route path="tests" element={<AdminDashboardTests />} />
+                        <Route path="lectures" element={<AdminDashboardLectures />} />
                     </Route>
                     <Route path="auth" element={<FormLayout />}>
                         <Route path="login" element={<Login />} />
